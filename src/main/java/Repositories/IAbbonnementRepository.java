7650585967f3af5b80c6547c9176ec920026a284
Aziz <aziz.abdellaoui16@gmail.com>
2023-06-08 00:36:09 +0100

    homeworkJPQL

--- src/main/java/Repositories/IAbbonnementRepository.java
+++ src/main/java/Repositories/IAbbonnementRepository.java
@@ -1,9 +1,22 @@
 package Repositories;
 
 import Entities.Abonnement;
+import Entities.Skier;
+import Entities.TypeAbonnement;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
 
 @Repository
 public interface IAbbonnementRepository extends JpaRepository<Abonnement, Long> {
+
+    @Query("SELECT abonnement FROM Abonnement abonnement WHERE abonnement.typeAbon=:x ORDER BY abonnement.dateDebut")
+    public List<Abonnement> getAbonnementsByTypeAbonOrderedByDateDebut(@Param("x") TypeAbonnement x);
+
+    @Query("SELECT abonnement FROM Abonnement abonnement WHERE abonnement.dateDebut BETWEEN :x and :y")
+    public List<Abonnement> getAbonnementBetweenTwoDates(@Param("x") Date x, @Param("y") Date y);
 }
--- src/main/java/Repositories/ICoursRepository.java
+++ src/main/java/Repositories/ICoursRepository.java
@@ -1,9 +1,19 @@
 package Repositories;
 
 import Entities.Cours;
+import Entities.Moniteur;
+import Entities.Support;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ICoursRepository extends JpaRepository<Cours, Long> {
+
+    @Query("SELECT DISTINCT i.numSemaine FROM Inscription i JOIN i.cours c WHERE c.moniteur = :moniteur AND c.support = :support")
+    List<Integer> findWeekNumbersByMoniteurAndSupport(@Param("moniteur") Moniteur moniteur, @Param("support") Support support);
+
 }
